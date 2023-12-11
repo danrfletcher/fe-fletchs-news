2afb4a3ed8d43394fafa62905d28efa114e90a63
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-11 16:48:24 +0000

    Creates static nav bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+import { List, Search } from 'react-bootstrap-icons'
+import fletchsNewsImage from '../assets/fletchs-news-mini-white.png';
+
+const Nav = styled.nav`
+    background-color: #1b1d1c;
+    color: #d1d2d2;
+    display: flex;
+    justify-content: space-between;
+    padding: 5px 15px;
+    align-items: center;
+    `
+const LogoAndCategories = styled.div``
+
+const MenuIcons = styled.div`
+    & > * {
+        margin-left: 15px;
+        height: 40px;
+        font-size: 1.5rem
+    }
+    `
+
+const Logo = styled.img`
+    max-width: 30vw
+    `
+
+export const NavBar = () => {
+    return (
+        <Nav>
+           <LogoAndCategories>
+                <Logo src={fletchsNewsImage} />
+           </LogoAndCategories>
+           <MenuIcons>
+                <Search />
+                <List />
+           </MenuIcons>
+        </Nav>
+    )
+}
