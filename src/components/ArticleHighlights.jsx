9bcc8c487f7c35678a8b68a23ffe72693d247e99
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-12 09:07:06 +0000

    Creates static ArticleHighlights component

--- src/components/ArticleHighlights.jsx
+++ src/components/ArticleHighlights.jsx
@@ -1,6 +1,25 @@
+import Card from 'react-bootstrap/Card';
+import styled from 'styled-components';
+
+const ArticleCard = styled(Card)`
+    margin: 15px;
+    border: none;
+    `
+
 export const ArticleHighlights = () => {
     return (
         <section>
+            <ArticleCard className="bg-dark text-white">
+                <Card.Img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" alt="Card image" />
+                <Card.ImgOverlay>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.
+                    </Card.Text>
+                    <Card.Text>Last updated 3 mins ago</Card.Text>
+                </Card.ImgOverlay>
+            </ArticleCard>
             <p>This will be the article highlights section.</p>
         </section>
     )
--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -8,7 +8,6 @@ export const Articles = () => {
         <>
             <Header />
             <main>
-                <ArticlesFilter />
                 <ArticleHighlights />
             </main>
             <Footer />
