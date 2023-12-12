2b7f1c6dc8a6b29d5c68769e60b23537da277b1f
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-12 16:49:03 +0000

    Adds League Spartan font & restructures CSS for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+import './css/App.css'
 import { Routes, Route } from 'react-router-dom'
 import { Articles } from './pages/Articles'
+import { Article } from './pages/Article'
 
 function App() {
   return (
     <Routes>
       <Route path='/articles' element={<Articles />} />
+      <Route path='/articles/:articleId' element={<Article />} />
     </Routes>
   )
 }
