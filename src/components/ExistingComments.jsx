a01604c0a6e1737521ed5742ea5566b63a8fcff0
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-13 08:54:39 +0000

    Completes logic for existing comments including creation time relative to today

--- src/components/ExistingComments.jsx
+++ src/components/ExistingComments.jsx
@@ -5,20 +5,49 @@ import { useFocusedArticle } from '../contexts/FocusedArticle';
 export const ExistingComments = () => {
     const {article} = useFocusedArticle();
     const [comments, setComments] = useState([]);
-
+    
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data = await fetchComments(article.article_id);
-                setComments(data);
-            } catch {
-                console.log("Error fetching existing comments for this article", error);
+        if (article.article_id) {
+            const fetchData = async () => {
+                try {
+                    const data = await fetchComments(article.article_id);
+                    setComments(data);
+                } catch {
+                    console.log("Error fetching existing comments for this article", error);
+                }
             }
+            fetchData();
         }
-    },[]);
+    },[article]);
 
-    console.log(comments);
+    const transformCommentPostTime = (createdAt) => {
+        const differenceInDays = Math.floor((new Date() - new Date(createdAt)) / (1000 * 60 * 60 * 24))
+
+        if ((differenceInDays / 365) > 1) {
+            return `${Math.floor(differenceInDays / 365)} years ago`
+        } else if (((differenceInDays / 31) > 1)) {
+            return `${Math.floor(differenceInDays / 31)} months ago`
+        } else if (((differenceInDays / 7) > 1)) {
+            return `${Math.floor(differenceInDays / 7)} weeks ago`
+        } else if (differenceInDays > 1) {
+            return `${differenceInDays} days ago`
+        } else {
+            return "today"
+        }
+    }
+
     return (
-        <p>Comments will go here</p>
+        <ul>
+            {comments.map((comment, index) => {
+                return (
+                    <li key={`${comment.author}-${index}`}>
+                        <p>@{comment.author}</p>
+                        <p>{transformCommentPostTime(comment.created_at)}</p>
+                        <p>{comment.body}</p>
+                        <p>{comment.votes}</p>
+                    </li>
+                )
+            })}
+        </ul>
     )
 }
