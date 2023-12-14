99eeff41f0d333351ab476020713f3f96672c301
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-14 14:02:23 +0000

    Provides logged in user context to entire app

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import './css/index.css'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FocusedArticleProvider } from './contexts/FocusedArticle';
+import { LoggedInUserProvider } from './contexts/LoggedInUser.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <FocusedArticleProvider>
-      <App />
-    </FocusedArticleProvider>
+    <LoggedInUserProvider>
+      <FocusedArticleProvider>
+        <App />
+      </FocusedArticleProvider>
+    </LoggedInUserProvider>
   </BrowserRouter>,
 )
