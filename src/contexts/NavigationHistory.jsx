732035a9cc331bc4d137fb22a0f18165f263fc79
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-16 22:37:44 +0000

    Adds navigation history context to nav user back to previous page after login

--- src/contexts/NavigationHistory.jsx
+++ src/contexts/NavigationHistory.jsx
@@ -1,14 +1,25 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import { useLocation } from 'react-router-dom';
+import { getToken, getProfileFromToken } from "../utils/token-api";
 
 const NavigationHistory = createContext();
 
 export const NavigationHistoryProvider = ({children}) => {
     const [page, setPage] = useState([]);
     const location = useLocation();
+    console.log("⚡ ~ page:", page)
+
+    const checkLogin = async () => {
+        console.log("Checking login...")
+        const accessToken = await getToken();
+        const userProfile = await getProfileFromToken();
+    }
 
     useEffect(() => {
         setPage(prev => [...prev, location.pathname])
+        if (page.length <= 1) {
+            checkLogin()
+        }
     },[location]);
     return (
         <NavigationHistory.Provider value={{page, setPage}}>
