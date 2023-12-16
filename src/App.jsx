136a6ed95eb4e21a32898022f51c8b10b10c59f0
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-16 18:35:43 +0000

    Adds error & successful login pages to routes for login

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import { Articles } from './pages/Articles'
 import { Article } from './pages/Article'
 import { Login } from './pages/Login'
+import { SuccessfulLogin } from './pages/SuccessfulLogin'
+import { Error } from './pages/Error'
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path='/articles' element={<Articles />} />
         <Route path='/articles/:articleId' element={<Article />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/login/success' element={<SuccessfulLogin />} />
+        <Route path='/error/:statusCode' element={<Error />} />
     </Routes>
   )
 }
