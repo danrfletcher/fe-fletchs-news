5005cdc086274f510ef90e7359380f2bcface014
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-17 13:46:49 +0000

    completes persistent session details api

--- src/utils/token-api.js
+++ src/utils/token-api.js
@@ -4,15 +4,32 @@ const baseURL = import.meta.env.VITE_API_BASE_URL
 
 export const getToken = async () => {
     try {
-        const token = await axios.post(`${baseURL}/api/token`, {
+        const token = await axios.get(`${baseURL}/api/token`, {
             withCredentials: true
         })
-        console.log("⚡ ~ token:", token)
+        if (token?.data?.accessToken) {
+            return token.data.accessToken;
+        }
     } catch (error) {
         return error
     }
 }
 
-export const getProfileFromToken = () => {
-
+export const getProfileFromToken = async (accessToken) => {
+    try {
+        if (accessToken) {
+            const userProfile = await axios.get(`${baseURL}/api/token/user`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                  }
+            })
+            if (userProfile) {
+                return userProfile.data.user
+            } else {
+                console.log("unauthorized access")
+            }
+        }
+    } catch (error) {
+        return error
+    }
 }
