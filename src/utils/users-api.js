37a7b58192bd24f0c3617e1982347cb153809e8a
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-17 13:46:30 +0000

    completes login api

--- src/utils/users-api.js
+++ src/utils/users-api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Cookie from 'js-cookie';
 
 const baseURL = import.meta.env.VITE_API_BASE_URL
 
@@ -14,6 +15,15 @@ export const fetchUsers = async (username) => {
 export const login = async (username, password) => {
     try {
         const success = await axios.post(`${baseURL}/api/users/login`, {username: username, password: password})
+        console.log("⚡ ~ success.data.refreshToken:", success.data.refreshToken)
+
+        const env = import.meta.env.CLIENT_ENV
+        Cookie.set('refreshToken', success.data.refreshToken, {
+            secure: env === 'development' || env === 'test' ? false : true, 
+            sameSite: env === 'development' || env === 'test' ? 'lax' : 'none', 
+            expires: new Date(new Date().setFullYear(new Date().getFullYear() + 10)),
+        });
+
         return success.data.accessToken
     } catch (error) {
         return error
