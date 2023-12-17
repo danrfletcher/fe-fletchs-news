573b2023a9fec5875003cb5e61c5173a15f4ec0b
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-17 15:43:47 +0000

    Adds route for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { Articles } from './pages/Articles'
 import { Article } from './pages/Article'
 import { Login } from './pages/Login'
 import { Error } from './pages/Error'
+import { Home } from './pages/Home'
 
 function App() {
   return (
     <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/articles' element={<Articles />} />
         <Route path='/articles/:articleId' element={<Article />} />
         <Route path='/login' element={<Login />} />
